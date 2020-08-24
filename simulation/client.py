8b10367ed9b8ccaea1899a58bdfb28ac31c5b35c
simonhuber95 <33151777+simonhuber95@users.noreply.github.com>
2020-08-24 17:27:18 +0200

    Add Serialization from Data set
    Add client creation from Data set ids
    Add reading from data set (not completet)

--- simulation/client.py
+++ simulation/client.py
@@ -3,24 +3,34 @@ import simpy
 
 
 class MobileClient(object):
-    def __init__(self, env, id):
+    def __init__(self, env, id, plan):
         self.env = env
         self.id = id
         self.phy_x = 0
         self.phy_y = 0
         self.virt_x = 0
         self.virt_y = 0
+        self.plan = plan
+        self.pairs = zip(plan.findall('activity'), plan.findall('leg'))
+
         print("Mobile client", self.id, "active")
       # Start the run process everytime an instance is created.
         self.action = env.process(self.run())
 
     def run(self):
-        movement = {"from": [0, 0], "to": [
-            8 *self.id, 20*self.id], "duration": 10 + self.id}
-        print("Client", self.id, "starts moving")
-        yield self.env.process(self.move(movement.get("to")[0], movement.get("to")[1], movement.get("duration")))
+        print("Client {} starting".format(self.id))
+        print(len(list(self.pairs)))
+        for activity, leg in self.pairs:
+            print(activity.get("type"))
+            entry = {}
+            entry['x'] = activity.attrib['x']
+            entry['y'] = activity.attrib['y']
+            route = leg.find('route')
+            entry['trav_time'] = route.attrib['trav_time']
+            entry['distance'] = route.attrib['distance']
+            yield self.env.process(self.move(entry['x'], entry['y'], entry['trav_time'], entry['distance']))
 
-    def move(self, to_x, to_y, duration):
+    def move(self, to_x, to_y, duration, distance):
         # velocity = math.sqrt((self.phy_x - to_x) ^ 2 + (self.phy_y - to_y) ^ 2)/duration  # in meter per tick
         vel_x = (to_x - self.phy_x) / duration
         vel_y = (to_y - self.phy_y) / duration
--- simulation/main.py
+++ simulation/main.py
@@ -1,13 +1,25 @@
 import simpy
 from client import MobileClient
 from node import FogNode
+import xml.etree.ElementTree as et
 
+client_path = "./data/berlin-v5.4-1pct.plans.xml"
 amount_clients = 1
+
+# Init Environment
 print("Init Environment")
 env = simpy.Environment()
+
+# Reading Clients from Open Berlin Scenario XML
+client_data = et.parse(client_path)
+
+# Looping over the first x entries
+for client in client_data.getroot().findall('person')[:amount_clients]:
+    client_id = client.get("id")
+    client_plan = client.find("plan")
+    client = MobileClient(env, client_id, client_plan)
+
 print("Init MobileClients")
-for i in range(1, amount_clients+1):
-    client = MobileClient(env, i)
 #print("Init Fog Nodes")
 #node = FogNode(env)
-env.run(until=20)
+env.run(until=30)
