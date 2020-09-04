79b5f534eada090dd2b6acd73aeb7f208c400cda
simonhuber95 <33151777+simonhuber95@users.noreply.github.com>
2020-09-04 12:26:23 +0200

    Add basic structure of node

--- simulation/main.py
+++ simulation/main.py
@@ -5,6 +5,9 @@ import xml.etree.ElementTree as et
 
 client_path = "./data/berlin-v5.4-1pct.plans.xml"
 amount_clients = 1
+amount_nodes = 1
+clients = []
+nodes = []
 
 # Init Environment
 print("Init Environment")
@@ -14,12 +17,17 @@ env = simpy.Environment()
 client_data = et.parse(client_path)
 
 # Looping over the first x entries
+print("Init Mobile Clients")
 for client in client_data.getroot().findall('person')[:amount_clients]:
     client_id = client.get("id")
     client_plan = client.find("plan")
-    client = MobileClient(env, client_id, client_plan)
+    client = MobileClient(env, id = client_id, plan = client_plan)
+    clients.append(client)
 
-print("Init MobileClients")
-#print("Init Fog Nodes")
-#node = FogNode(env)
+print("Init Fog Nodes")
+for node_id in range(1, amount_nodes+1):
+    node = FogNode(env, id = node_id, discovery_protocol={}, network = {}, slots = 1)
+    nodes.append(node)
+    
+# Run Simulation
 env.run(until=2500)
--- simulation/node.py
+++ simulation/node.py
@@ -1,10 +1,28 @@
+from simpy import Resource
+
 class FogNode(object):
-    def __init__(self, env):
+    def __init__(self, env, id, discovery_protocol, network, slots):
         self.env = env
-      # Start the run process everytime an instance is created.
+        self.id = id
+        self.discovery_protocol = discovery_protocol
+        self.network = network
+        self.resource = Resource(env, slots)
+        
+        # Start the run process everytime an instance is created.
         self.action = env.process(self.run())
+        print("Mobile client {} active, current location x: {}, y: {}".format(self.id, self.phy_x, self.phy_y))
 
     def run(self):
         while True:
             print('I am a fog node')
             yield self.env.timeout(5)
+            
+    def connect(self, client):
+        with self.resource.request as req:
+            yield req
+            yield self.env.timeout(1)
+            
+    # returns closest node relative to client
+    def get_closest(self, client):
+        # ToDo
+        print("Closest node is {}")
