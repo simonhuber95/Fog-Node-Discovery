72e82527e2b428c1b31656ea9811adc1130cf1d2
simonhuber95 <33151777+simonhuber95@users.noreply.github.com>
2020-09-14 11:43:39 +0200

    Add multiple processes for Client

--- simulation/client.py
+++ simulation/client.py
@@ -10,6 +10,8 @@ class MobileClient(object):
         self.id = id
         self.plan = plan
         self.connected = False
+        # Event triggers search for closest node
+        self.req_node_event = env.event()
         self.msg_pipe = simpy.Store(env)
         # Set coordinates to first activity in plan
         self.phy_x = float(plan.find('activity').attrib["x"])
@@ -20,61 +22,61 @@ class MobileClient(object):
         self.virt_y = 0
         print("Client {}: active, current location x: {}, y: {}".format(
             self.id, self.phy_x, self.phy_y))
-        # Start the run process everytime an instance is created.
-        self.action = env.process(self.run())
         self.connected_node = False
-
-    def run(self):
-        print("Client {}: starting".format(self.id))
-        # Finding closest node process from by connecting to random node
-        closest_node = yield self.env.process(self.req_closest_node())
-
-        # Connecting to closest node
-        yield self.env.process(self.connect(closest_node))
-
+        
+        # Starting the operating processes
+        self.connect_process = self.env.process(self.connect())
+        self.request_process = self.env.process(self.req_closest_node())
+        self.move_process = self.env.process(self.move())    
+        
+    def move(self):
+        print("Client {}: starting move Process".format(self.id))
         # Iterate through every leg & activity in seperate process
         for activity, leg in self.pairs:
             entry = self.get_entry_from_data(activity=activity, leg=leg)
+            duration = entry['trav_time']
+            distance = entry['distance']
+            to_x = entry['x']
+            to_y = entry['y']
             print(entry)
-            yield self.env.process(self.move(entry['x'], entry['y'], entry['trav_time'], entry['distance']))
 
-    def move(self, to_x, to_y, duration, distance):
-        # Calculating the deltas in each direction
-        # the order is (latitude, longitude) or (y, x) in Cartesian terms
-        # dist_x = geo_distance.distance((0, self.phy_x), (0, to_x))
-        # dist_y = geo_distance.distance((self.phy_y, 0), (to_y, 0))
-        dist_x = to_x - self.phy_x
-        dist_y = to_y - self.phy_y
-        vel_x = dist_x / duration
-        vel_y = dist_y / duration
-        while(round(to_x, 2) != round(self.phy_x, 2) and round(to_y, 2) != round(self.phy_y, 2)):
-            self.phy_x += vel_x
-            self.phy_y += vel_y
+            # Calculating the deltas in each direction
+            # the order is (latitude, longitude) or (y, x) in Cartesian terms
+            # dist_x = geo_distance.distance((0, self.phy_x), (0, to_x))
+            # dist_y = geo_distance.distance((self.phy_y, 0), (to_y, 0))
+            dist_x = to_x - self.phy_x
+            dist_y = to_y - self.phy_y
+            vel_x = dist_x / duration
+            vel_y = dist_y / duration
+            while(round(to_x, 2) != round(self.phy_x, 2) and round(to_y, 2) != round(self.phy_y, 2)):
+                self.phy_x += vel_x
+                self.phy_y += vel_y
 
-            # if (self.connected_node == False):
-            # self.req_closest_node() ToDo
-            # print("Client {}: Not connected to network".format(self.id))
-            yield self.env.timeout(1)
+                yield self.env.timeout(1)
 
-            # print("Timestep: {} Client id: {} x:{:.2f} y:{:.2f}".format(
-            #     self.env.now, self.id, self.phy_x, self.phy_y))
-            # print("Delta x: {}, Delta y: {}".format(round(to_x - self.phy_x, 2), round(to_y - self.phy_y, 2)))
+                print("Timestep: {} Client id: {} x:{:.2f} y:{:.2f}".format(
+                    self.env.now, self.id, self.phy_x, self.phy_y))
+                # print("Delta x: {}, Delta y: {}".format(round(to_x - self.phy_x, 2), round(to_y - self.phy_y, 2)))
 
     def req_closest_node(self):
-        while(not self.connected):
+        print("Client {}: starting Request closest node Process".format(self.id))
+        while(True):
+            yield self.req_node_event # passivate the search for closest node. Is triggered if reconnection Criteria is fullfilled
             # Emit Event to any node of the Fog Network to retrieve closest node
             print("Client {}: Probing network".format(self.id))
             node = self.env.getNode(random.choice(range(1, len(self.env.nodes)+1)))
             node.probe_event.succeed(
                 {"client_id": self.id, "msg_pipe": self.msg_pipe})
             node.probe_event = self.env.event()
-            # Receiving Message from radnom node
+            # Receiving Message from random node
             in_msg = yield self.msg_pipe.get()
             print("Client {}: Nearest node is {}".format(self.id, in_msg))
 
-    def connect(self, node_id):
+    def connect(self):
+        print("Client {}: starting Connection Process".format(self.id))
         # Connect to closest node of the Fog Network
         print("ToDo")
+        yield self.env.timeout(1)
 
     def get_entry_from_data(self, activity, leg):
         entry = {}
