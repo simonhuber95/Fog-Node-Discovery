4d5cbe4ecbae87de33039d4438ea018c67b92ad5
simonhuber95 <33151777+simonhuber95@users.noreply.github.com>
2020-10-20 16:39:16 +0200

    Add VivaldiPosition class

--- vivaldi/vivaldiposition.py
+++ vivaldi/vivaldiposition.py
@@ -1,10 +1,23 @@
-MAX_X = 30000
-MAX_Y = 30000
-MAX_H = 30000
+from heightcoodinates import HeightCoordinates
+
+CONVERGE_EVERY = 5
+CONVERGE_FACTOR = 50
+ERROR_MIN = 0.1
+
+cc = 0.25
+ce = 0.5
+initial_error = 10
+
 
 class VivaldiPosition(object):
     """
     docstring
     """
-    def __init__():
-        pass
+
+    def __init__(self, coords):
+    	if not isinstance(coords, HeightCoordinates):
+			throw TypeError('Argument 1 must be a HeightCoordinates');
+
+		self._coordinates = coords
+		self._error = initial_error
+		self._nbUpdates = 0
