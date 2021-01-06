84edef6d72f6ef2d80a918d7d9593f2c375431e9
simonhuber95 <33151777+simonhuber95@users.noreply.github.com>
2021-01-06 18:06:39 +0100

    Update fog_environment.py

--- simulation/fog_environment.py
+++ simulation/fog_environment.py
@@ -12,6 +12,13 @@ import time
 
 class FogEnvironment(Environment):
     def __init__(self, config):
+        """Child object of simpy.Environment, implements a FogEnvironment
+        Has list of clients, nodes and celltowers
+        Runs a monitor process
+
+        Args:
+            config (dict): Dictionary of the config.yml file
+        """
         super().__init__()
         self.config = config
         self.clients = []
@@ -62,49 +69,32 @@ class FogEnvironment(Environment):
         return message
 
     def message_delivery(self, message):
+        """A delivery process for the message
+        Waits the latency of the message and then puts the message into the receicer's message pipe
+
+        Args:
+            message (Message): Message to be delivered
+
+        Yields:
+            simpy.timeout: Delivery process waits the given latency of the message
+        """
         yield self.timeout(message.latency)
         self.get_participant(message.rec_id).msg_pipe.put(message)
 
     def get_latency(self, send_id, rec_id):
-        """
-        Parameter send_id as string: ID of sender
-        Paramater rec_id as string: ID of recipient
-        Returns float: Latency in seconds
+        """Calculates the latency between two participants in the network
+
+        Args:
+            send_id (uuid): ID of sender
+            rec_id (uuid): ID of recipient
+
+        Returns:
+            float: Latency in seconds
         """
         my_random = Random()
         my_random.seed(str(self.now) + str(send_id) + str(rec_id))
         sender = self.get_participant(send_id)
         receiver = self.get_participant(rec_id)
-
-        # Single hop/Device2Device Connection via 5G bands
-        if self.config["simulation"]["connection"] == "single-hop":
-            distance = self.get_distance(
-                sender.phy_x, sender.phy_y, receiver.phy_x, receiver.phy_y)
-            high_band_distance = self.config["bands"]["5G-High"]["distance"]
-            medium_band_distance = self.config["bands"]["5G-Medium"]["distance"]
-            low_band_distance = self.config["bands"]["5G-Low"]["distance"]
-
-            # Deviaton formular: (distance - Distmin)/(Distmax -Distmin) * (MaxDev - MinDev) + MinDev
-            # Squashes the distance from the participant between [MinDev, MaxDev] and is multiplies with the standard latency
-            # High-Band 5G
-            if(distance < high_band_distance):
-                high_deviation = (distance - 0) / \
-                    (high_band_distance - 0) * (1.25 - 0.75) + 0.75
-                return self.config["bands"]["5G-High"]["latency"]/1000 * my_random.randint(90, 110)/100 * high_deviation
-            # Medium-Band 5G
-            elif (distance < medium_band_distance):
-                medium_deviation = (distance - high_band_distance)/(
-                    medium_band_distance - high_band_distance) * (1.25 - 0.75) + 0.75
-                return self.config["bands"]["5G-Medium"]["latency"]/1000 * my_random.randint(90, 110)/100 * medium_deviation
-            # Low-Band 5G
-            elif (distance < low_band_distance):
-                low_deviation = (distance - medium_band_distance) / \
-                    (low_band_distance - medium_band_distance) * \
-                    (1.25 - 0.75) + 0.75
-                return self.config["bands"]["5G-Low"]["latency"]/1000 * my_random.randint(90, 110)/100 * low_deviation
-            # 3G
-            else:
-                return 0.15
 
         # Latency calculation for multihop between client and node connection is the following:
         # Latency = Sum ( Transmission delay + Propagation + Processing + Queuing )
@@ -112,37 +102,36 @@ class FogEnvironment(Environment):
         # Propagation = distance km * 0.0035 ms/km
         # Processing = [0.010, 0.030]ms + Network error (= constant 0.5ms) -> depending on Hardware
         # Queing = 1 / (1 * bandwidth Gbps) with upper limit of 5ms
-        elif self.config["simulation"]["connection"] == "multi-hop":
 
-            # Connection between 2 nodes the less good bandwidth is used
-            if isinstance(sender, FogNode) and isinstance(receiver, FogNode):
+        # Connection between 2 nodes the less good bandwidth is used
+        if isinstance(sender, FogNode) and isinstance(receiver, FogNode):
 
-                bandwidth = min(sender.get_bandwidth(),
-                                receiver.get_bandwidth())
-                transmission_delay = -0.008 * bandwidth + 0.088
-                # basically no distance as we are connected via backhaul
-                distance = self.get_distance(sender.phy_x, sender.phy_y, receiver.phy_x, receiver.phy_y)/1000
-                propagation_delay = distance * 0.0035
-                processing_delay = sender.hardware * 0.01 + 0.05
-                queuing_delay = min(50, 1/(2 * bandwidth))
-                # print(transmission_delay + propagation_delay + processing_delay + queuing_delay, distance)
-            # Connection between client and node
-            else:
-                # Checking which participant is Node and who is Client
-                client = sender if isinstance(
-                    sender, MobileClient) else receiver
-                node = sender if isinstance(sender, FogNode) else receiver
+            bandwidth = min(sender.get_bandwidth(),
+                            receiver.get_bandwidth())
+            transmission_delay = -0.008 * bandwidth + 0.088
+            # basically no distance as we are connected via backhaul
+            distance = self.get_distance(sender.phy_x, sender.phy_y, receiver.phy_x, receiver.phy_y)/1000
+            propagation_delay = distance * 0.0035
+            processing_delay = sender.hardware * 0.01 + 0.05
+            queuing_delay = min(50, 1/(2 * bandwidth))
+            # print(transmission_delay + propagation_delay + processing_delay + queuing_delay, distance)
+        # Connection between client and node
+        else:
+            # Checking which participant is Node and who is Client
+            client = sender if isinstance(
+                sender, MobileClient) else receiver
+            node = sender if isinstance(sender, FogNode) else receiver
 
-                # Calculating the physical distance from each participant to the cell tower
-                celltower_id_cl, distance_cl = self.get_nearest_celltower(client)
-                celltower_id_n, distance_n = self.get_nearest_celltower(node)
-                distance = distance_cl + distance_n
-                transmission_delay = -0.008 * node.get_bandwidth() + 0.088
-                propagation_delay = distance/1000 * 0.0035
-                processing_delay = node.hardware * 0.01 + 0.05
-                queuing_delay = min(50, 1/(2 * node.get_bandwidth()))
+            # Calculating the physical distance from each participant to the cell tower
+            celltower_id_cl, distance_cl = self.get_nearest_celltower(client)
+            celltower_id_n, distance_n = self.get_nearest_celltower(node)
+            distance = distance_cl + distance_n
+            transmission_delay = -0.008 * node.get_bandwidth() + 0.088
+            propagation_delay = distance/1000 * 0.0035
+            processing_delay = node.hardware * 0.01 + 0.05
+            queuing_delay = min(50, 1/(2 * node.get_bandwidth()))
 
-            return (transmission_delay + propagation_delay + processing_delay + queuing_delay)/1000
+        return (transmission_delay + propagation_delay + processing_delay + queuing_delay)/1000
 
     def get_distance(self, send_x, send_y, rec_x, rec_y):
         """Calculates the physical distance between to points in meters
@@ -160,6 +149,15 @@ class FogEnvironment(Environment):
         return distance
 
     def get_message(self, msg_id):
+        """Gets the message object of a given message ID
+        Info: Message List currently gets emptied every simulated second by the monitor process
+
+        Args:
+            msg_id (uuid): Id of the message
+
+        Returns:
+            Message: The message with the given ID or None if no message is found
+        """
         return next((message for message in self.messages if message.id == msg_id), None)
 
     def generate_boundaries(self, x_trans, y_trans, method="center"):
@@ -234,7 +232,7 @@ class FogEnvironment(Environment):
                 latencies.append(
                     {"id": node["id"], "lat": lat, 'slots': node["obj"].slots, 'clients': len(node["obj"].clients)})
 
-        # Secondary sort by ID, primary sort b latency
+        # Primary sort by latency, secondary sort by ID
         latencies = sorted(latencies, key=itemgetter('id'))
         sorted_lat = sorted(latencies, key=itemgetter('lat'))
         # When there is no node with an open slot we return None
@@ -243,32 +241,40 @@ class FogEnvironment(Environment):
             return None
 
         closest_node = sorted_lat.pop(0)
-        # print("closest node {} has a current workload of {}".format(closest_node.get("id"), closest_node.get('clients')/closest_node.get('slots')))
         return closest_node.get("id")
 
     def monitor(self):
+        """Monitor process
+        Prints the current progress of the simulation every simulated second
+        Clears the message List every second to save memory
+
+        """
         runtime = self.config["simulation"]["runtime"]
         modulus = runtime / 10
         timestamp = 0
         while(True):
-
-            # if(self.now == 0):
-            #     print("Runtime: {}/{}".format(self.now, runtime))
-            # elif(self.now % modulus == 0):
-            #     print("Runtime: {}/{}".format(self.now, runtime))
             duration = round(time.perf_counter() - timestamp, 2)
             timestamp = time.perf_counter()
             print("Runtime: {}/{} in {} seconds with {} messages".format(self.now,
                                                                          runtime, duration, len(self.messages)))
-            # print("Runtime: {}/{} in {} seconds with".format(self.now, runtime, duration))
+
             # clear message history
             self.messages = []
             yield self.timeout(1)
 
-    def get_nearest_celltower(self, client):
+    def get_nearest_celltower(self, participant):
+        """Searches the geographically closest cell tower for a given participant
+
+        Args:
+            participant (MobileClient): The participant for which the nearest cell tower is searched
+
+        Returns:
+            uuid: ID of the cell tower
+            float: Distance between the cell tower and the participant
+        """
         celltowers = []
         for celltower in self.celltowers:
-            a_x, a_y = client.get_coordinates()
+            a_x, a_y = participant.get_coordinates()
             b_x, b_y = celltower["obj"].get_coordinates()
             dist = self.get_distance(a_x, a_y, b_x, b_y)
             celltowers.append({"id": celltower.get('id'), "distance": dist})
